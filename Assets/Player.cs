efe2fb81a26e69d637dba92bae689fd83d834d88
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2024-05-05 15:57:49 +0100

    Local multiplayer scenes

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   [SerializeField] private float speed = 200f;
+   [SerializeField] private string inputNameHorizontal;
+   [SerializeField] private string inputNameVertical;
+
+   private Rigidbody rb;
+ 
+   private float inputHorizontal;
+   private float inputVertical;
+
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   private void Update()
+   {
+      inputHorizontal = Input.GetAxis(inputNameHorizontal);
+      inputVertical = Input.GetAxis(inputNameVertical);
+      
+   }
+   
+   private void FixedUpdate()
+   {
+      rb.velocity = new Vector3(inputHorizontal * speed * Time.fixedDeltaTime, rb.velocity.y, inputVertical * speed * Time.fixedDeltaTime);
+   }
+}
